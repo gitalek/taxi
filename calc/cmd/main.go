120a1b35d824b1e7d76fd183b330a3f3b969fe30
Alexander <shalek.edu@mail.ru>
2021-01-19 22:00:32 +1000

    calc: refactor project structure: add config and server packages

--- calc/cmd/main.go
+++ calc/cmd/main.go
@@ -1,63 +1,23 @@
 package main
 
 import (
-	"fmt"
-	calc "github.com/gitalek/taxi/calc/pkg"
-	"github.com/go-kit/kit/log"
-	httptransport "github.com/go-kit/kit/transport/http"
-	"github.com/gorilla/mux"
-	l "log"
-	"net/http"
+	"github.com/gitalek/taxi/calc/config"
+	"github.com/gitalek/taxi/calc/server"
+	"github.com/spf13/viper"
+	"log"
 )
 
-const port = "9090"
-
 func main() {
-	var svc calc.Service
-	svc = &calc.CalcService{}
-	loggerSvc := log.NewLogfmtLogger(log.StdlibWriter{})
-	loggerSvc = log.WithPrefix(loggerSvc, "app", "calc", "layer", "logic")
-	svc = calc.AppLoggingMiddleware{
-		Logger: loggerSvc,
-		Next:   svc,
+	// reading config data into viper
+	err := config.Init()
+	if err != nil {
+		log.Fatalf("error while reading config: %#v\n", err)
 	}
 
-	loggerEndpoint := log.NewLogfmtLogger(log.StdlibWriter{})
-	calculatePrice := calc.MakeCalculatePriceEndpoint(svc)
-	calculatePrice = calc.LoggingMiddleware(
-		log.WithPrefix(loggerEndpoint, "app", "calc", "layer", "transport: endpoint", "method", "calculatePrice"),
-	)(calculatePrice)
-
-	r := mux.NewRouter()
-	r.Methods("POST").
-		Path("/calcprice").
-		Handler(
-			httptransport.NewServer(
-				calculatePrice,
-				calc.DecodeRequest,
-				calc.EncodeResponse,
-			),
-		)
-
-	r.Methods("POST").
-		Path("/v2/calcprice").
-		Handler(
-			httptransport.NewServer(
-				calculatePrice,
-				calc.DecodeRequestV2,
-				calc.EncodeResponse,
-			),
-		)
-
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprint(w, "Taxi application")
-		if err != nil {
-			fmt.Printf("calc: handler: '/': error while writing response: %#v\n", err)
-		}
-	})
-
-	address := fmt.Sprintf(":%s", port)
-	l.Printf("Starting server at port %s\n", port)
-	http.Handle("/", r)
-	l.Fatal(http.ListenAndServe(address, nil))
+	app := server.NewApp()
+	// todo приведение типа?
+	port := viper.GetString("port")
+	if err := app.Run(port); err != nil {
+		log.Fatalf("error while running server: %#v", err.Error())
+	}
 }
--- go.mod
+++ go.mod
@@ -4,9 +4,8 @@ go 1.15
 
 require (
 	github.com/go-kit/kit v0.10.0
-	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/gorilla/mux v1.7.3
+	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.6.1
 	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7
-	google.golang.org/protobuf v1.25.0 // indirect
 )
